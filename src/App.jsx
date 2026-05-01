023c5065cdd6a8b0c7490865fe19291b04aa35d1
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-01 17:26:14 +0500

    Refactor App component for improved structure and readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,35 @@
 // import { useState } from 'react'
-import './App.css'
-import  Navbar from '../src/components/Navbar';
-import  Searchbar from '../src/components/Searchbar';
-import  WeatherStates from '../src/components/WeatherStates';
-import  WeatherCard from '../src/components/WeatherCard';
-import  Dailyforcast from '../src/components/Dailyforcast';
-import  Hourlyforcast from '../src/components/Hourlyforcast';
+import "./App.css";
+import Navbar from "../src/components/Navbar";
+import Searchbar from "../src/components/Searchbar";
+import WeatherStates from "../src/components/WeatherStates";
+import WeatherCard from "../src/components/WeatherCard";
+import Dailyforcast from "../src/components/Dailyforcast";
+import Hourlyforcast from "../src/components/Hourlyforcast";
 function App() {
- 
-
   return (
     <>
-    <Navbar/>
-    <Searchbar />
-    <WeatherCard/>
-    <WeatherStates/>
-    <Dailyforcast/>
-    <Hourlyforcast/>
+      <div className="min-h-screen px-4 py-6">
+        <div className="max-w-7xl mx-auto space-y-8">
+          <div className="flex flex-col gap-6">
+            <Navbar />
+            <Searchbar />
+          </div>
 
-      
+          <section className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div className="flex flex-col gap-6 col-span-2">
+              <WeatherCard />
+              <WeatherStates />
+              <Dailyforcast />
+            </div>
+            <div className="col-span-1">
+              <Hourlyforcast />
+            </div>
+          </section>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
