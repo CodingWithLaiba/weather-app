6e774335ebad77980faaa551ef499fdf6a0bd1d2
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-02 16:53:40 +0500

    Refactor Navbar component for improved structure and readability

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import settingIcon from "../assets/images/icon-units.svg;"
+import dropdownIcon from "../assets/images/icon-dropdown.svg";
 function Navbar() {
   return (
     <div>
-      
+      <nav className="flex items-center justify-between p-4">
+        <img src={logo} alt="Weather App" />
+        <button className="dropdown toggle">
+          <img src={settingIcon} alt="settingicon" />
+          <span>Units</span>
+          <img src={dropdownIcon} alt="dropdownicon" />
+
+        </button>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
