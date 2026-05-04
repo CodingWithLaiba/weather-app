af328ceee04864830edc339913a4ae301b3e695f
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-04 22:52:43 +0500

    Refactor Navbar component to destructure props and improve code readability

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,3 @@
-import React  from "react";
-
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 import logo from "../assets/images/logo.svg";
@@ -7,27 +5,32 @@ import settingIcon from "../assets/images/icon-units.svg";
 import dropdownIcon from "../assets/images/icon-dropdown.svg";
 import tickIcon from "../assets/images/icon-checkmark.svg";
 
-function Navbar(temperatureUnit, setTemperatureUnit,windUnit,setWindUnit,precipitationUnit,setPrecipitationUnit) {
+function Navbar({
+  temperatureUnit,
+  setTemperatureUnit,
+  windUnit,
+  setWindUnit,
+  precipitationUnit,
+  setPrecipitationUnit,
+}) {
   const handleSwitch = () => {
     setTemperatureUnit("f");
     setWindUnit("mph");
     setPrecipitationUnit("in");
   };
-
+console.log(setTemperatureUnit);
   return (
     <nav className="flex items-center justify-between py-4">
-      {/* Logo */}
       <img src={logo} alt="Weather App" />
 
-      {/* Dropdown */}
       <Menu as="div" className="relative">
-        <MenuButton className="inline-flex justify-between items-center cursor-pointer gap-2 rounded-lg bg-[hsl(243,23%,30%)] px-2 py-2 text-xs text-white hover:bg-[hsl(243,23%,35%)]">
+        <MenuButton className="inline-flex items-center gap-2 rounded-lg bg-[hsl(243,23%,30%)] px-2 py-2 text-xs text-white hover:bg-[hsl(243,23%,35%)]">
           <img src={settingIcon} alt="Settings" className="w-3 h-3" />
           Units
           <img src={dropdownIcon} alt="Dropdown" className="w-3 h-3" />
         </MenuButton>
 
-        <MenuItems className="absolute right-0 mt-2 w-45 rounded-xl bg-[hsl(243,23%,30%)] text-white shadow-lg cursor-pointer divide-y divide-gray-600 px-1">
+        <MenuItems className="absolute right-0 mt-2 w-48 rounded-xl bg-[hsl(243,23%,30%)] text-white shadow-lg divide-y divide-gray-600 p-1">
           {/* Switch */}
           <MenuItem>
             {({ active }) => (
@@ -44,15 +47,13 @@ function Navbar(temperatureUnit, setTemperatureUnit,windUnit,setWindUnit,precipi
 
           {/* Temperature */}
           <div className="mt-2">
-            <p className="px-2 py-1 text-xs text-gray-500 ">
-              Temperature
-            </p>
+            <p className="px-2 py-1 text-xs text-gray-400">Temperature</p>
 
             <MenuItem>
               {({ active }) => (
                 <button
                   onClick={() => setTemperatureUnit("c")}
-                  className={`flex justify-between items-center w-full px-2 py-2 text-sm ${
+                  className={`flex justify-between w-full px-2 py-2 text-sm ${
                     active ? "bg-[hsl(243,23%,35%)]" : ""
                   }`}
                 >
@@ -68,7 +69,7 @@ function Navbar(temperatureUnit, setTemperatureUnit,windUnit,setWindUnit,precipi
               {({ active }) => (
                 <button
                   onClick={() => setTemperatureUnit("f")}
-                  className={`flex justify-between items-center w-full px-2 py-2 text-sm ${
+                  className={`flex justify-between w-full px-2 py-2 text-sm ${
                     active ? "bg-[hsl(243,23%,35%)]" : ""
                   }`}
                 >
@@ -83,15 +84,13 @@ function Navbar(temperatureUnit, setTemperatureUnit,windUnit,setWindUnit,precipi
 
           {/* Wind */}
           <div className="mt-2">
-            <p className="px-2 py-1 text-xs text-gray-400 ">
-              Wind Speed
-            </p>
+            <p className="px-2 py-1 text-xs text-gray-400">Wind Speed</p>
 
             <MenuItem>
               {({ active }) => (
                 <button
                   onClick={() => setWindUnit("km/h")}
-                  className={`flex justify-between items-center w-full px-2 py-2 text-sm ${
+                  className={`flex justify-between w-full px-2 py-2 text-sm ${
                     active ? "bg-[hsl(243,23%,35%)]" : ""
                   }`}
                 >
@@ -107,7 +106,7 @@ function Navbar(temperatureUnit, setTemperatureUnit,windUnit,setWindUnit,precipi
               {({ active }) => (
                 <button
                   onClick={() => setWindUnit("mph")}
-                  className={`flex justify-between items-center w-full px-2 py-2 text-sm ${
+                  className={`flex justify-between w-full px-2 py-2 text-sm ${
                     active ? "bg-[hsl(243,23%,35%)]" : ""
                   }`}
                 >
@@ -122,15 +121,13 @@ function Navbar(temperatureUnit, setTemperatureUnit,windUnit,setWindUnit,precipi
 
           {/* Precipitation */}
           <div className="mt-2">
-            <p className="px-2 py-1 text-xs text-gray-400 ">
-              Precipitation
-            </p>
+            <p className="px-2 py-1 text-xs text-gray-400">Precipitation</p>
 
             <MenuItem>
               {({ active }) => (
                 <button
                   onClick={() => setPrecipitationUnit("mm")}
-                  className={`flex justify-between items-center w-full px-2 py-2 text-sm ${
+                  className={`flex justify-between w-full px-2 py-2 text-sm ${
                     active ? "bg-[hsl(243,23%,35%)]" : ""
                   }`}
                 >
@@ -146,7 +143,7 @@ function Navbar(temperatureUnit, setTemperatureUnit,windUnit,setWindUnit,precipi
               {({ active }) => (
                 <button
                   onClick={() => setPrecipitationUnit("in")}
-                  className={`flex justify-between items-center w-full px-2 py-2 text-sm ${
+                  className={`flex justify-between w-full px-2 py-2 text-sm ${
                     active ? "bg-[hsl(243,23%,35%)]" : ""
                   }`}
                 >
