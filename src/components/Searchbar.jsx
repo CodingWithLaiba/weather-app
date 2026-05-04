1a7e999c98a30614aeb3d4acc64fd86414f1f22e
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-04 22:57:47 +0500

    Fix typo in handleSearch function and restore Searchbar component structure

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -2,13 +2,27 @@ import { useState } from "react"
 
 function Searchbar({setCity}) {
   const [input, setInput] = useState("");
-  const handleSerach = () =>{
+  const handleSearch = () =>{
     if(!input.trim()) return;
     setCity(input); 
   }
-  return (
-    <div>searchbar</div>
-  )
+   return (
+    <div className="flex gap-2">
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Search city..."
+        className="px-4 py-2 rounded-lg w-full"
+      />
+
+      <button
+        onClick={handleSearch}
+        className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+      >
+        Search
+      </button>
+    </div>
+  );
 }
 
-export default Searchbar
+export default Searchbar;
