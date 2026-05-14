578cf8affe80410fab5d6653d04849469b740593
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-14 19:52:09 +0500

    Update WeatherCard component to display current temperature with proper formatting

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+function WeatherCard({ data, convertTemperature, temperatureUnit }) {
+  if (!data) return <p>Loading...</p>;
 
-function WeatherCard() {
+  const temp = data.current_weather.temperature;
+
   return (
-    <div>weathercard</div>
-  )
+    <div>
+      <h1>
+        {convertTemperature(temp).toFixed(1)}°{temperatureUnit.toUpperCase()}
+      </h1>
+    </div>
+  );
 }
-
-export default WeatherCard
+export default WeatherCard;
