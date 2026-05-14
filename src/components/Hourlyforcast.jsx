527405f3cc598d7a375dd67f2926e8c65783cce1
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-14 19:51:59 +0500

    Fix Hourlyforcast component to correctly render hourly weather data

--- src/components/Hourlyforcast.jsx
+++ src/components/Hourlyforcast.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+function Hourlyforcast({ data }) {
+  if (!data?.hourly) return null;
 
-function Hourlyforcast() {
   return (
     <div>
-      
+      {data.hourly.time.slice(0, 6).map((time, i) => (
+        <div key={i}>
+          {time} - {data.hourly.temperature_2m[i]}°
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Hourlyforcast
