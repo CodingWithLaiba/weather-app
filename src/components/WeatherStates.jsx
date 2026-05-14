94fe0819f4298052b0d93fc080f22c0608ba80e0
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-14 19:52:23 +0500

    Update WeatherStates component to display wind speed with proper conversion

--- src/components/WeatherStates.jsx
+++ src/components/WeatherStates.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
 
-function WeatherStates() {
+
+function WeatherStates(data, convertWindSpeed,windUnit) {
+    if(!data) return null;
+    
   return (
-    <div>weatherstates</div>
+    <div>
+      <p>
+        Wind: {convertWindSpeed(data.current_weather.windspeed).toFixed(1)} {windUnit}
+      </p>
+    </div>
   )
 }
 
