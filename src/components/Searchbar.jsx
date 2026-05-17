085d9759e8ef1667599de1159f14b0104a6729f8
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-17 19:47:21 +0500

    Refactor Searchbar component for improved layout and input styling

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -13,25 +13,25 @@ function Searchbar({ setCity }) {
   };
 
   return (
-    <div className="space-y-4 flex flex-col items-center justify-center gap-4">
+    <div className="space-y-4 flex flex-col items-center justify-center gap-8">
       <h1 className="text-3xl md:text-5xl font-bold text-white text-center">
         How's the sky looking today?
       </h1>
-      <form
+      <form 
         onSubmit={handleSubmit}
-        className="flex items-center gap-3"
+        className="flex items-center gap-3 w-135"
       >
         <input
           type="text"
           placeholder="Search for a place..."
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          className="w-full rounded-xl bg-[hsl(243,23%,30%)] px-4 py-3 text-white outline-none placeholder:text-gray-400"
+          className="w-full h-11 rounded-xl bg-[hsl(243,23%,30%)] px-4 py-3 text-white outline-none placeholder:text-gray-400"
         />
 
         <button
           type="submit"
-          className="rounded-xl bg-[hsl(233,67%,56%)] px-6 py-3 text-white font-medium hover:bg-[hsl(233,67%,46%)] transition-colors"
+          className="rounded-xl h-11 bg-[hsl(233,67%,56%)] px-6 py-3 text-white font-medium hover:bg-[hsl(233,67%,46%)] transition-colors"
         >
           Search
         </button>
