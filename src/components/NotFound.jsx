a0c846ba0a9a1343a758cedfdb15afcbd711ef14
Laiba Ijaz <laibaijaz904@gmail.com>
2026-05-21 15:59:41 +0500

    Refactor NotFound component to simplify message and remove city-specific details

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,12 +1,10 @@
-
-export default function NotFound({city}) {
+export default function NotFound() {
   return (
-    <div className="bg-[hsl(243,23%,30%)] border border-white/5 rounded-3xl min-h-[350px] flex flex-col items-center justify-center text-center px-6">
-
+    <div>
       {/* Title */}
-      <h2 className="text-white text-3xl font-semibold">
-        No Search results found for {city}!
+      <h2 className="text-white text-center text-xl font-semibold">
+        No search results found!
       </h2>
-      </div>
-  )
+    </div>
+  );
 }
